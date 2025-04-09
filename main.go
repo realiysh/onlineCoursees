bd369b4bc09ef94464cee4ed1a6dbade295daff4
DataStorageApp <alisherperzentbayev@MacBook-Air-Aliser-2.local>
2025-04-09 17:56:51 +0500

    Initial commit

--- main.go
+++ main.go
@@ -1,28 +1,21 @@
 package main
 
 import (
-	"WelcomeGo/handlers"
-
 	"github.com/gin-gonic/gin"
+	"project1/database"
+	"project1/routes"
 )
 
 func main() {
+	// Инициализируем базу данных
+	database.ConnectDB()
+
+	// Создаём сервер Gin
 	r := gin.Default()
 
-	// Кітап маршруты
-	r.GET("/books", handlers.GetBooks)
-	r.GET("/books/:id", handlers.GetBookByID)
-	r.POST("/books", handlers.AddBook)
-	r.PUT("/books/:id", handlers.UpdateBook)
-	r.DELETE("/books/:id", handlers.DeleteBook)
+	// Регистрируем маршруты
+	routes.RegisterRoutes(r)
 
-	// Автор маршруты
-	r.GET("/authors", handlers.GetAuthors)
-	r.POST("/authors", handlers.AddAuthor)
-
-	// Категория маршруты
-	r.GET("/categories", handlers.GetCategories)
-	r.POST("/categories", handlers.AddCategory)
-
+	// Запускаем сервер
 	r.Run(":8080")
 }
--- models/author.go
+++ models/author.go
@@ -1,6 +1,6 @@
 package models
 
 type Author struct {
-	ID   int    `json:"id"`
+	ID   uint   `json:"id"`
 	Name string `json:"name"`
 }
